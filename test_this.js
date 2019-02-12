fb9a0e081ad789cbed680e07b0f57eecf2bd126c
DDran <286823247@qq.com>
2019-02-12 11:03:20 +0800

    myFirst Git

---
+++ test_this.js
@@ -0,0 +1,24 @@
+function LList() {
+    this.head = new Node('头部');
+    console.log("this.head.element",this.head.element);
+    this.find = find;
+    //console.log(this.find);
+    //this.insert = insert;
+    //this.remove = remove;
+    //this.display = display;
+}
+
+function Node(element) {
+    this.element = element;
+    console.log("this.element",this.element);
+    //this.next = null;
+}
+
+function find(){
+    var ifo = this.head.element;
+    console.log("find方法中的ifo",ifo);
+    return ifo;
+}
+
+var a =new LList();
+a.find();
